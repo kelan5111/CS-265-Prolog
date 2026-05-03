12bb848adea4445a95f630ead3312931b3abefd9
Kelan Westwood <kelanwestwood51@gmail.com>
2026-05-03 18:17:58 +0100

    Added how and why.

--- KB_rules.pl
+++ KB_rules.pl
@@ -10,7 +10,6 @@ these represent the rules to find the facts in the predicate set:
     fact(a)
     fact(b)
 
-
 q is our goal (conclusion)
 */
 
@@ -19,7 +18,41 @@ is_true(P) :- if Condition then P, is_true(Condition).
 is_true(P1 and P2) :- is_true(P1), is_true(P2).
 is_true(P1 or P2) :- is_true(P1); is_true(P2).
 
+/* Defining if, then, and, or which currently do not exist in prolog*/
+:- op(800, fx, if).
+:- op(700, xfx, then).
+:- op(600, xfy, and).
+:- op(600, xfy, or).
+
+
+% Reasoning WHY and HOW
+how(P) :-
+    fact(P),
+    write(P), write(' is a fact '), nl.
+
+how(P) :-
+    if Condition then P,
+    write(P), write(' because '), write(Condition), nl,
+    how(Condition).
+
+why(P) :-
+      fact(P),
+      write(P), write(' is a known fact'), nl.
+
+  why(P) :-
+      if Condition then P,
+      write('To prove '), write(P),
+      write(' we need '), write(Condition), nl,
+      why(Condition).
+
 
 /* ------- Knowledge Base ----------*/
-
-
+% Facts
+fact(hallway_wet).
+fact(bathroom_dry).
+fact(window_closed).
+% Rules
+if hallway_wet and bathroom_dry then leak_bathroom.
+if hallway_wet and bathroom_dry then problem_kitchen.
+if window_closed or no_rain then no_water_outside.
+if problem_kitchen and no_water_outside then leak_in_kitchen.
