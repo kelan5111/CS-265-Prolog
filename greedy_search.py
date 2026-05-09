f2e431ec047adb8718620f05b3c84393b3c4ea55
Kelan Westwood <kelanwestwood51@gmail.com>
2026-05-09 15:05:27 +0100

    Added greedy_search.py.

---
+++ greedy_search.py
@@ -0,0 +1,111 @@
+#################################
+####    Greedy Search application on Search Problem
+###     @kelan5111
+#################################
+
+
+class GreedySearch():
+    def __init__(self, problem=None, goal=None):
+
+        if problem is None: return
+
+        try:
+            self.set_problem(problem)
+
+            self._curr_action = None
+            self._curr_depth = 0
+            self._frontier = []
+
+            if goal != None: self._goal = goal
+
+        except TypeError as e:
+            print("Should be type Dic however got " + e)
+
+
+    def execute_search(self):
+        while(True):
+            if self._problem is None: 
+                print("No problem to solve.")
+                break
+            
+            # 1. Goal test: return true or false
+            found = self._goal_test(self._curr_node["state"])
+            if found:
+                print("Success")
+                break
+            elif len(self._frontier) == 0 and not found:
+                print("Failure.")
+                break
+
+            # 2. Expand func: successor func & forming nodes
+            self._expand()
+
+            # 3. Search: heuristic which is highest
+            next_node = self._search()
+
+            if next_node != None:
+                self._curr_node = next_node
+                self._curr_action = next_node["action"]
+
+    def _search(self):
+        next_node = None
+
+        for node in self._frontier:
+            if next_node == None:
+                next_node = node
+                continue
+            
+            if node["heuristic"] <= next_node["heuristic"]:
+                next_node = node
+
+        # Remove the next node from frontier
+        self._frontier.remove(next_node)
+
+        # Checking if node is child (depth)
+        if next_node["parent"] == self._curr_node["state"]:
+            self._curr_depth += 1
+        else:
+            self._curr_depth -= 1
+        
+        return next_node
+    
+    def _calc_heuristic(self, node):
+        return self._goal - node
+
+    def _expand(self):
+       # 1. Expanding current node
+       children = self._get_successors(self._curr_node["state"])
+
+       # 2. Creating nodes
+       for action, state in children:
+            new_node = {
+                "state": state,
+                "parent": self._curr_node,
+                "action": action,
+                "path_cost": self._get_path_cost(state),
+                "heuristic": self._calc_heuristic(state)
+            }
+            self._frontier.append(new_node) # New node added to frontier
+        
+
+    def set_problem(self, problem):
+        self._problem = problem
+
+        # Setting Initial State as Current State
+        self._curr_node = {
+            "state": problem["initial_state"],
+            "parent": None,
+            "action": None,
+            "path_cost": 0,
+            "heuristic": self._calc_heuristic(problem["initial_state"])
+        }
+        
+        self._goal_test = problem["goal_test"]  # Returns: True/False
+        self._get_successors = problem["successor_func"]    # Returns Dict: action-state pairs
+        self._get_path_cost = problem["path_cost"]  # Returns: path cost of a specified state
+
+
+
+def _successor_func(self):
+        # Expanding current node children and adding to frontier
+        return self._successors[self._curr_node]
